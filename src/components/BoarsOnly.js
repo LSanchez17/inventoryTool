7a42a3af342ebb22869d9233289ef2d60ea5b7c6
LSanchez17 <galaxyplayeremail@gmail.com>
2021-05-29 23:55:59 -0400

    split up some components, state needs saved separately

--- src/components/BoarsOnly.js
+++ src/components/BoarsOnly.js
@@ -1,7 +1,24 @@
+import InventoryItem from './InventoryItem'
+
 const BoarsOnly = ({chubs}) => {
+    console.log(chubs);
+    
+    let meatChub = chubs.map((chub,idx) => {
+        let className = `${chub.type} grid grid-cols-1 gap-2 place-items-center`
+        return <div key={idx} className={className}>
+                    <InventoryItem key={idx} 
+                              identifier={idx}
+                              type={chub.type}
+                              name={chub.name} 
+                              weight={chub.weight} 
+                              upc={chub.upc}
+                              count={chub.count} />
+                </div>
+    });
+
     return(
     <>
-    {chubs}
+    {meatChub}
     </>
     )
 }
--- src/components/displayChubs.js
+++ src/components/displayChubs.js
@@ -1,27 +1,9 @@
 import PrivateOnly from './PrivateOnly';
 import BoarsOnly from './BoarsOnly';
-import InventoryItem from './InventoryItem'
 import {useState, useEffect} from 'react'
 
 const DisplayChubs = ({chubs}) => {
-    let meatChub = chubs.map((chub,idx) => {
-        let className = `${chub.type} grid grid-cols-1 gap-2 place-items-center`
-        return <div key={idx} className={className}>
-                    <InventoryItem key={idx} 
-                              identifier={idx}
-                              type={chub.type}
-                              name={chub.name} 
-                              weight={chub.weight} 
-                              upc={chub.upc}
-                              count={chub.count} />
-                </div>
-      });
-
-    //save rendered children for iteration
-    //default renders every item
-    let INIT = meatChub;
-    let [sections, setSections] = useState(INIT);
-    //keeps track of both lists when split
+    //split the chubs into these two
     let [boarsHead, setBoarsHead] = useState();
     let [privateSelection, setPrivateSelection] = useState();
     //keeps track of which to show and hide
@@ -33,11 +15,11 @@ const DisplayChubs = ({chubs}) => {
         let bh = [];
         let ps = [];
 
-        sections.forEach(chub => {
-            if(chub.props.children.props.type === 'PS'){
+        chubs.forEach(chub => {
+            if(chub.type === 'PS'){
                 ps.push(chub);
             }
-            if(chub.props.children.props.type === 'BH'){
+            if(chub.type === 'BH'){
                 bh.push(chub);
             }
         });
@@ -48,20 +30,20 @@ const DisplayChubs = ({chubs}) => {
     
     const showPS = () => {
         //update state and save it
-        setPrivateOnly(privateSelection => !privateSelection);
+        setPrivateOnly(privateOnly => !privateOnly);
     }
 
     const showBH = () => {
         //update state and save it
-        setBoarsOnly(boarsHead => !boarsHead);
+        setBoarsOnly(boarsOnly => !boarsOnly);
     }
 
     return (
         <div>
             <button className='bg-black text-yellow-300 rounded-2xl border-4 p-2 m-2' onClick={showBH}>Show Boar's Head</button>
             <button className='bg-blue-500 text-white rounded-2xl border-4 p-2 m-2' onClick={showPS}>Show Private Selection</button>
-            {privateOnly ? <PrivateOnly chubs={privateSelection} /> : sections}
-            {boarsOnly ? <BoarsOnly chubs={boarsHead} /> : sections}
+            {privateOnly ? <PrivateOnly chubs={privateSelection}/> : ''}
+            {boarsOnly ? <BoarsOnly chubs={boarsHead}/> : ''}
         </div>
     );
 }
