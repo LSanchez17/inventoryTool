2b30e5c76dc440155151b36bfe5cc136cb55395f
LSanchez17 <galaxyplayeremail@gmail.com>
2021-06-07 20:16:34 -0400

    added new items

--- src/helpers/chubs.js
+++ src/helpers/chubs.js
@@ -13,6 +13,7 @@ let chubs = [
     {type:'BH', name: 'BH Deluxe Ham', weight: 9.75, upc: "0004242161100", count: 0},{type:'BH', name: 'BH BlackForest Ham', weight: 7.03, upc: "0004242161434", count: 0},
     {type:'BH', name: 'BH L/S Ham', weight: 4.31, upc: "000424216115", count: 0},{type:'BH', name: 'BH Sweet Slice', weight: 5.21, upc: "000424216114", count: 0},
     {type:'BH', name: 'BH Bourbon Ham', weight: 3.21, upc: "0004242161460", count: 0},{type:'BH', name: 'BH London Broil', weight: 5, upc: "0004242161411", count: 0},
+    {type:'BH', name: 'BH L/S Roast Beef', weight: 4.97, upc: "0004242161134", count: 0},
     {type:'BH', name: 'BH Corn Beef', weight: 5.97, upc: "0004242161125", count: 0},{type:'BH', name: 'BH Seasoned Beef', weight: 5.68, upc: "000424216273", count: 0},
     {type:'BH', name: 'BH Pastrami', weight: 5.8, upc: "0004242161126",count: 0},{type:'BH', name: 'BH Hard Salami', weight: 2.7, upc: "0004242161213", count: 0},
     {type:'BH', name: 'BH Genoa Salami', weight: 3.8, upc: "0004242161212", count: 0},{type:'BH', name: 'BH Pepperoni', weight: 3.4, upc: "0004242161214", count: 0},
@@ -32,11 +33,15 @@ let chubs = [
     {type: 'PS', name: 'PS Nashville Chicken', weight: 5, upc: "0001111019274",count: 0},{type: 'PS', name: 'PS Black Forest Ham', weight: 4.25, upc: "00011111099024", count: 0},
     {type: 'PS', name: 'PS Roast Beef', weight: 6.5, upc: "00011111060263",count: 0},{type: 'PS', name: 'PS Brown Sugar Ham', weight: 4.25, upc: "00011111099415",count: 0},
     {type: 'PS', name: 'PS CherryWood Ham', weight: 5, upc: "0001111011213",count: 0},{type: 'PS', name: 'PS Polish Ham', weight: 7, upc: "00011111011501", count: 0},
+    {type: 'PS', name: 'PS Maple Ham', weight: 4.5, upc: "0001111011500", count: 0},
     {type: 'PS', name: 'PS L/S Chicken', weight: 5.5, upc: "00011111058667",count: 0},{type: 'PS', name: 'PS Tavern Ham', weight: 4.5, upc: "0001111105867",count: 0},
     {type: 'PS', name: 'PS Genoa Salami', weight: 3.75, upc: "00011111060273", count: 0},{type: 'PS', name: 'PS Hard Salami', weight: 3.75, upc: "00011111060274",count: 0},
     {type: 'PS', name: 'PS Beef Bologna', weight: 3.4, upc: "00011111052230",count: 0},{type: 'PS', name: 'PS Pepperoni', weight: 3.125, upc: "00011111099838",count: 0},
     {type: 'PS', name: 'PS Yellow American', weight: 5, upc: "00011111058673",count: 0},{type: 'PS', name: 'PS White American', weight: 5, upc: "00011111058674",count: 0},
     {type: 'PS', name: 'PS Baby Swiss', weight: 7, upc: "00011111060715",count: 0},{type: 'PS', name: 'PS Colby Jack', weight: 6.25, upc: "00011111060716",count: 0},
-    {type: 'PS', name: 'PS Colby', weight: 6.25, upc: "00011111060719",count: 0},{type: 'PS', name: 'PS Provolone', weight: 6, upc: "00011111060721",count: 0}]
+    {type: 'PS', name: 'PS Colby', weight: 6.25, upc: "00011111060719",count: 0},{type: 'PS', name: 'PS Provolone', weight: 6, upc: "00011111060721",count: 0},
+    {type: 'PS', name: 'PS Aged Gouda', weight: 3, upc: "0001111058364", count: 0},{type:'PS', name: 'PS Havarti', weight: 4.5, upc: "0001111000274", count: 0},
+    {type: 'PS', name: 'PS Muenster', weight: 6, upc: "0001111000041", count: 0},{type: 'PS', name: 'PS PepperJack', weight: 4.235, upc: "0001111060717", count: 0}
+]
 
 export default chubs;
