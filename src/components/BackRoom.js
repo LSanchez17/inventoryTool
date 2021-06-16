0d97bbf888dd101c778560f4141d43bb5895ea60
LSanchez17 <galaxyplayeremail@gmail.com>
2021-06-15 21:40:01 -0400

    setup for backroom items

---
+++ src/components/BackRoom.js
@@ -0,0 +1,10 @@
+
+const BackRoom = ({backRoomItems}) => {
+    return (
+        <div>
+            <p>Backroom items here!</p>
+        </div>
+    );
+};
+
+export default BackRoom;
--- src/components/displayChubs.js
+++ src/components/displayChubs.js
@@ -1,5 +1,6 @@
 import PrivateOnly from './PrivateOnly';
 import BoarsOnly from './BoarsOnly';
+import BackRoom from './BackRoom';
 import {useState, useEffect} from 'react'
 
 const DisplayChubs = ({chubs}) => {
@@ -9,6 +10,8 @@ const DisplayChubs = ({chubs}) => {
     //keeps track of which to show and hide
     let [privateOnly, setPrivateOnly] = useState(false);
     let [boarsOnly, setBoarsOnly] = useState(false);
+    //handles backroom itesm
+    let [backRoom, setBackRoom] = useState(false);
 
     useEffect(() => {
         //split main chub list into two
@@ -38,12 +41,19 @@ const DisplayChubs = ({chubs}) => {
         setBoarsOnly(boarsOnly => !boarsOnly);
     }
 
+    const showBackRoom = () => {
+        //show backroom items
+        setBackRoom(backRoom => !backRoom);
+    }
+
     return (
         <div>
+            <button className='bg-gray text-white rounded-2xl border-4 p-2 m-2' onClick={showBackRoom}>Show Backroom</button>
             <button className='bg-black text-yellow-300 rounded-2xl border-4 p-2 m-2' onClick={showBH}>Show Boar's Head</button>
             <button className='bg-blue-500 text-white rounded-2xl border-4 p-2 m-2' onClick={showPS}>Show Private Selection</button>
             {privateOnly ? <PrivateOnly chubs={privateSelection} show={privateOnly}/> : ''}
             {boarsOnly ? <BoarsOnly chubs={boarsHead} show={boarsOnly}/> : ''}
+            {backRoom ? <BackRoom backRoomItems={backRoom} /> : ''}
         </div>
     );
 }
